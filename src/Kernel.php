4bab68145d49da476427ce20d4970adaf4c55b56
vega <aziaha.formations@gmail.com>
2024-03-16 13:19:39 +0100

    sprint2-ticket1-feat: registration system implemented

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,20 @@
 
 namespace App;
 
+use App\Trait\ChangeTimeZoneTrait;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    use ChangeTimeZoneTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+        
+        $this->changeTimeZone($_ENV['TIME_ZONE']);
+
+        parent::__construct($environment, $debug);
+    }
 }
